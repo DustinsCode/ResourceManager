ea1e168b768e887f47bf7926e2ec5cd5b4161f51
Dustin Thurston <dthurston1996@gmail.com>
2018-11-09 10:04:08 -0500

    added deadlock and test files

--- ResMan.py
+++ ResMan.py
@@ -2,6 +2,9 @@
 import sys
 import matplotlib.pyplot as plt
 import networkx as nx
+import warnings
+warnings.filterwarnings("ignore",".*GUI is implemented.*")
+
 
 PAUSE_VAL = 1
 
@@ -14,15 +17,18 @@ class ResMan:
         """
         initialize variables
         """
-        self.fileName = ""
-        self.contents = []
-        self.processes = 0
-        self.resources = 0
-        self.resourceList = []
-        self.usedResources = []
-        self.processDict = {}
-        self.waitingList = []
-        self.G = nx.DiGraph()
+        self.fileName = ""          # name of file parameter
+        self.contents = []          # contents of file
+        self.processes = 0          # num of processes
+        self.resources = 0          # num of resources
+        self.resourceList = []      # list of resources
+        self.usedResources = []     # list of resources in use
+        self.processDict = {}       # dictionary of processes and the resources they own
+        self.waitingList = []       # list of tuples of processes waiting on a res
+        self.lockedProcesses = []   # list of locked processes
+        self.deadlocked = False     # is the system deadlocked?
+        self.G = nx.DiGraph()       # the graph
+
         # ensuring arguments entered
         if len(sys.argv) > 1:
             self.fileName = sys.argv[1]
@@ -30,7 +36,6 @@ class ResMan:
         else:
             print("Please enter a data file argument.")
             exit(1)
-        # readFile(self, fileName)
 
     def readFile(self):
         """
@@ -38,7 +43,7 @@ class ResMan:
         instantiates class variables
         """
         file = open(self.fileName, "r")
-        self.contents = file.read().split('\r\n')
+        self.contents = file.read().split('\n')
 
         self.processes = int(self.contents[0][0])
         self.resources = int(self.contents[1][0])
@@ -46,7 +51,6 @@ class ResMan:
         # clean up to allow for easier reading of remaining values
         self.contents.pop(0)
         self.contents.pop(0)
-        # print(self.contents)
 
         # create list of resources for graphing purposes
         for i in range(0, self.resources):
@@ -61,16 +65,17 @@ class ResMan:
 
     def draw(self):
         """
-        Draws the current state
+        Draws the current state & checks for deadlock
         """
+        # clears old data before drawing again
         plt.clf()
         plt.axis('off')
-        # G = nx.DiGraph()
+
         # get list of processes and resources
         processes = self.processDict.keys()
 
         # get edges (list of tuples)
-        edges = []
+        edges = []      # Owned resources
         for key in self.processDict:
             for value in self.processDict[key]:
                 edges.append((value,key))
@@ -79,10 +84,9 @@ class ResMan:
         # print("edges: ", edges)
         # print("waitingList: ", self.waitingList)
 
+        # Read nodes/edges into networkx and draw
         self.G.add_nodes_from(processes + self.resourceList)
-        # self.G.add_edges_from(edges, color='g')
         self.G.add_edges_from(edges+self.waitingList)
-        # Read nodes into networkx
         pos = nx.bipartite_layout(self.G, nodes=processes,align='horizontal')
         nx.draw_networkx(self.G, pos, nodelist=self.resourceList,
                             node_color='y',node_size=600,with_labels=True,
@@ -92,6 +96,27 @@ class ResMan:
                             node_color='r',node_size=600,with_labels=True,
                             label="Processes",edgelist=self.waitingList,edge_color='b',
                             alpha=1)
+
+        # Check for deadlock!
+        # get list of cycles from networkx graph
+        cycles = list(nx.simple_cycles(self.G))
+        # print("cycles: ",cycles)
+        # print("processes: ", self.processDict.keys())
+
+        for cycleList in cycles:
+            # get nodes from cycle list
+            if len(cycleList) > 2:
+                for node in cycleList:
+                    if node in self.processDict.keys() and node not in self.lockedProcesses:
+                        self.lockedProcesses.append(node)
+        # check for system deadlock
+        if len(self.lockedProcesses) == self.processes:
+            self.deadlocked = True
+            return
+        # else list locked processes
+        elif len(self.lockedProcesses) > 0:
+            print("Locked Processes: ", self.lockedProcesses)
+
         plt.waitforbuttonpress()
 
     def step(self):
@@ -105,41 +130,41 @@ class ResMan:
         self.contents.pop(0)
         print(thisStep)
 
-        proc = thisStep[0]
-        cmd = thisStep[1]
-        res = thisStep[2]
+        proc = thisStep[0]  # current process
+        cmd = thisStep[1]   # current command
+        res = thisStep[2]   # current resource
 
-        # print("Nodes in G: ", self.G.nodes(data=True))
-        # print("Edges in G: ", self.G.edges(data=True))
-        # make sure resource is not already held
-        if cmd == "requests":
+        # if the process is locked, it can't do anything
+        if proc not in self.lockedProcesses:
             # handle requests
-            if res in self.usedResources:
-                self.waitingList.append((proc, res))
-                self.draw()
-                return
+            if cmd == "requests":
 
-            self.processDict[proc].append(res)
-            self.usedResources.append(res)
-        elif cmd == "releases":
-            # Assume if trying to release, the process has control already
-            self.G.remove_edge(res,proc)
-            self.processDict[proc].remove(res)
-            self.usedResources.remove(res)
-            self.draw()
-            # search waiting list for that resource
-            for tuple in self.waitingList:
-                if res in tuple:
-                    self.processDict[tuple[0]].append(res)
+                # make sure resource is not already held
+                if res in self.usedResources:
+                    self.waitingList.append((proc, res))
+
+                else:
+                    self.processDict[proc].append(res)
                     self.usedResources.append(res)
-                    self.waitingList.remove(tuple)
-                    #redraw to show the step of another process taking its turn
-                    self.draw()
-                    # plt.pause(PAUSE_VAL)
-                    break
+                    print(proc+ " now owns "+ res)
+            # handle releases
+            elif cmd == "releases":
+                # Assume if trying to release, the process has control already
+                self.G.remove_edge(res,proc)
+                self.processDict[proc].remove(res)
+                self.usedResources.remove(res)
+                print(proc + " released " + res)
 
-        # print(self.processDict)
-        self.draw()
+                # search waiting list for that resource
+                for tuple in self.waitingList:
+                    if res in tuple:
+                        self.draw()
+                        self.processDict[tuple[0]].append(res)
+                        self.usedResources.append(res)
+                        self.waitingList.remove(tuple)
+                        break
+
+            self.draw()
 
     def main(self):
         self.readFile()
@@ -148,9 +173,18 @@ class ResMan:
         plt.ion()
         # plt.legend()      looks weird  TODO: fix if I have time
         plt.show()
+
+        # loop to run the manager
         for i in range(0,len(self.contents)-1):
-            self.step()
-            # plt.waitforbuttonpress()
+            if self.deadlocked:
+                print("Deadlocked.  Cannot continue.")
+                self.deadlocked = False
+                break
+            else:
+                self.step()
+
+        if self.deadlocked:
+            print("Deadlocked. Cannot continue.")
         raw_input("Press enter to quit..")
 
 
